8fcd2f40ee0be67b1c5ca9f34165ae60e83fa4b2
Shadowninja33 <38512435+Shadowninja33@users.noreply.github.com>
2024-07-29 20:33:10 -0500

    Pull in real photos

--- src/lib/data/art-pieces.ts
+++ src/lib/data/art-pieces.ts
@@ -1,39 +1,72 @@
 import sunset from '$lib/images/sunset.webp';
+import aDaysWork from '$lib/images/art/a-days-work.jpg';
+import dorieMillerMural from '$lib/images/art/dorie-miller-mural.jpg';
+import emilyMorgan from '$lib/images/art/emily-morgan.jpg';
+import fatherAndChild from '$lib/images/art/father-and-child.jpg';
+import muralAndArtist from '$lib/images/art/mural-and-artist.jpg';
+import musician from '$lib/images/art/musician.jpg';
+import nursing from '$lib/images/art/nursing.jpg';
+import seamstress from '$lib/images/art/seamstress.jpg';
+import woman from '$lib/images/art/woman.jpg';
+import sisters from '$lib/images/art/sisters.jpg';
+import sisters2 from '$lib/images/art/sisters-2.jpg';
 
 export const artPieces = [
-    {
-        id: '1af5b845-0dcc-42a8-a9bf-2de147e629a4',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-    {
-        id: 'a114897f-465a-41d3-9ebf-02d3fe878d3b',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-    {
-        id: '9f5d59a6-5240-4ada-bf41-08ebbc8d8b0f',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-    {
-        id: '9b098b7b-f02c-4eeb-995d-8c3723245d47',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-    {
-        id: 'ad0c879a-399b-4358-b8f8-e21a749a6d0a',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-    {
-        id: '7282c6ae-1a3b-43e5-a0ae-2b328f6d9855',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-    {
-        id: '6f13b85b-b5e7-49a6-a3da-c35a05f28237',
-        src: sunset,
-        alt: 'A beautiful sunset on the coast'
-    },
-]
+	{
+		id: '1af5b845-0dcc-42a8-a9bf-2de147e629a4',
+		src: aDaysWork,
+		alt: 'A Painting of a boy smiling while holding oranges, surrounded by fruit',
+		name: "A Day's Work"
+	},
+
+	{
+		id: '9f5d59a6-5240-4ada-bf41-08ebbc8d8b0f',
+		src: emilyMorgan,
+		alt: 'A Painting of Emily Morgan (a free woman of color) in a peach color dress holding a yellow rose'
+	},
+	{
+		id: '9b098b7b-f02c-4eeb-995d-8c3723245d47',
+		src: fatherAndChild,
+		alt: 'A profile painting of a father holding his child on his shoulders and smiling'
+	},
+	{
+		id: '6f13b85b-b5e7-49a6-a3da-c35a05f28237',
+		src: nursing,
+		alt: 'A painting of a woman of color nursing her baby'
+	},
+	{
+		id: '4fa0d45e-0f24-49bf-8043-2d08b8d0ec99',
+		src: woman,
+		alt: 'A sculpture of a woman of color wearing a white and red dress, blue eyeshadow, and red lipstick'
+	},
+	{
+		id: 'ad0c879a-399b-4358-b8f8-e21a749a6d0a',
+		src: muralAndArtist,
+		alt: 'A picture of Jacqui Dorsey (the artist) standing next to a mural she painted featuring 3 women of color'
+	},
+	{
+		id: 'c6988a2e-4a68-4c64-afb6-1151cc7a5715',
+		src: seamstress,
+		alt: 'A sculpture of a colorfully dressed seamstress wearing a thimble and holding a piece of string'
+	},
+	{
+		id: '2c075af7-392b-4c6e-902b-dafa307f82d0',
+		src: sisters,
+		alt: 'A painting of two sisters wearing straw color hats. The first woman is looking over her shoulder towards the viewer, while the other faces away.'
+	},
+	{
+		id: 'c6988a2e-4a68-4c64-afb6-1151cc7a5715',
+		src: sisters2,
+		alt: 'A painting of two sisters wearing straw color hats. The second woman is looking over her shoulder towards the viewer, while the first faces away.'
+	},
+	{
+		id: 'a114897f-465a-41d3-9ebf-02d3fe878d3b',
+		src: dorieMillerMural,
+		alt: 'A mural featuring Dorie Miller and other influential black figures'
+	},
+	{
+		id: '7282c6ae-1a3b-43e5-a0ae-2b328f6d9855',
+		src: musician,
+		alt: 'A sculpture of a well-dressed violinist playing his instrument'
+	}
+];
