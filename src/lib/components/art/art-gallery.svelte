9072d623ad449046b1fc1679c450d20f665d317c
shadowninja33 <prestondcobb@gmail.com>
2026-02-16 13:39:52 -0600

    Use default images for art-gallery

--- src/lib/components/art/art-gallery.svelte
+++ src/lib/components/art/art-gallery.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { artPieces } from '$lib/data/art-pieces';
+	import { defaultImage } from '$lib/utils/art-piece';
 
 	export let pieces: typeof artPieces;
 </script>
@@ -7,7 +8,11 @@
 <section class="grid grid-cols-2 md:grid-cols-4 gap-4">
 	{#each pieces as piece}
 		<a href="/{piece.id}" class="hover:brightness-50">
-			<img class="h-auto max-w-full rounded-lg" src={piece.src} alt={piece.alt} />
+			<img
+				class="h-auto max-w-full rounded-lg"
+				src={defaultImage(piece).src}
+				alt={defaultImage(piece).alt}
+			/>
 		</a>
 	{/each}
 </section>
