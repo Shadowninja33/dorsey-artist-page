b109761a1c2901ef792d4b1bae74d1ee4ca82762
shadowninja33 <prestondcobb@gmail.com>
2026-02-16 13:36:00 -0600

    Remove extra about section text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,35 +5,8 @@
 	<p>
 		Jacqui Dorsey is an accomplished Sculptor, Muralist, retired nurse and painter, whose work has
 		won many awards throughout the nation. Her three-dimensional clay works are sensitive endearing
-		narratives of family, friends, and historical figures. Her clay bust, "The Way To Freedom was
+		narratives of family, friends, and historical figures. Her clay bust, "The Way To Freedom" was
 		shown at the Pittsburgh Center for the Arts. Artist Jacqui Dorsey is actively involved in the
 		community and served as a docent at McNay Museum of Arts.
-	</p>
-	<p>
-		Curabitur id ipsum et tellus condimentum vehicula ut non tellus. Pellentesque mollis massa in
-		sagittis consectetur. Phasellus vel metus gravida dui bibendum hendrerit nec sit amet metus.
-		Mauris elementum vitae urna sed pretium. Donec gravida quam sit amet interdum porttitor. Nam
-		pulvinar eget massa sit amet mollis. Vivamus sed bibendum dolor. Pellentesque vel viverra arcu.
-		Vivamus tempus ligula turpis, eu mattis justo commodo a.
-	</p>
-	<p>
-		Vestibulum ac nulla a ex bibendum ultricies. Vivamus laoreet, lorem eget semper efficitur, dui
-		velit blandit risus, sodales tristique mi magna sed eros. Duis ullamcorper augue justo, ac
-		viverra nulla tristique nec. Quisque sollicitudin, nibh id feugiat dapibus, nisi sem luctus
-		massa, quis blandit justo justo sit amet elit. Nullam ut risus non velit pulvinar cursus a sed
-		justo. Vivamus tincidunt mauris quis eleifend sollicitudin. Donec placerat sodales diam at
-		porta. Nunc semper lorem vitae vulputate pretium. Vestibulum lacinia posuere quam. Duis
-		imperdiet dictum posuere. Donec congue sapien eu orci consectetur dictum a sit amet mauris.
-		Vivamus et tristique est. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	</p>
-	<p>
-		Ut sagittis erat dui, sed ullamcorper lorem faucibus in. Duis mollis pharetra faucibus. Nulla
-		bibendum tempus libero, eget molestie ipsum lobortis vitae. Donec id metus at mauris vestibulum
-		cursus. Vivamus aliquam dignissim nisi, eu pharetra quam porta quis. Ut auctor, diam sit amet
-		fringilla placerat, sem tortor molestie ex, et mattis quam sem eu metus. Integer in ipsum eget
-		leo consectetur sagittis sit amet sed neque. Cras eu mi felis. Cras ut dui eu quam tincidunt
-		porta. Donec eu bibendum ipsum. Pellentesque suscipit luctus neque, ac efficitur nunc euismod
-		molestie. Suspendisse ex mauris, accumsan sit amet consectetur nec, dapibus vitae sem. Nulla sit
-		amet purus vel dui sollicitudin accumsan.
 	</p>
 </article>
