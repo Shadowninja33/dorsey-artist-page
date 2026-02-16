08c7476b3aacd0ae0075c2bad3fdb1b7225d9919
shadowninja33 <prestondcobb@gmail.com>
2026-02-16 13:40:19 -0600

    Refine art piece page

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,14 +1,19 @@
-import { error } from "@sveltejs/kit";
-import { artPieces } from "$lib/data/art-pieces";
+import { error } from '@sveltejs/kit';
+import { artPieces } from '$lib/data/art-pieces';
 
-export function load({params}) {
-    const pieceIndex = artPieces.findIndex((art) => art.id === params.id);
-    const piece = artPieces[pieceIndex];
-    
-    if (!piece) throw error(404);
+export function load({ params }: { params: { id: string } }) {
+	const pieceIndex = artPieces.findIndex((art) => art.id === params.id);
+	const piece = artPieces[pieceIndex];
 
-    return {
-        pieceIndex,
-        piece
-    }
+	const previousArtPiece = pieceIndex - 1 >= 0 ? artPieces[pieceIndex - 1] : null;
+	const nextArtPiece = pieceIndex + 1 < artPieces.length ? artPieces[pieceIndex + 1] : null;
+
+	if (!piece) throw error(404);
+
+	return {
+		pieceIndex,
+		piece,
+		previousArtPiece,
+		nextArtPiece
+	};
 }
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
+	import { generateGoogleMapsSearchUrl } from '$lib/utils/google-maps';
+
 	export let data;
 
-	$: previousArtPiece = data.pieceIndex - 1 >= 0 ? data.artPieces[data.pieceIndex - 1] : null;
-	$: nextArtPiece =
-		data.pieceIndex + 1 < data.artPieces.length ? data.artPieces[data.pieceIndex + 1] : null;
 	const noop: string = 'javascript:;';
+
+	$: selectedImageIndex = 0;
+	$: selectedImage = data.piece.images[selectedImageIndex];
 </script>
 
-<nav class="flex justify-between px-8 pt-4">
+<nav class="flex justify-between px-8 py-4">
 	<div class="flex justify-start">
-		<a class="underline" href={previousArtPiece?.id || noop}>Previous</a><span class="mx-1">/</span
-		><a class="underline" href={nextArtPiece?.id || noop}>Next</a>
+		{#if data.previousArtPiece?.id}
+			<a class="underline" href={data.previousArtPiece.id}>Previous</a>
+		{:else}
+			<span>Previous</span>
+		{/if}
+		<span class="mx-1">/</span>
+		{#if data.nextArtPiece?.id}
+			<a class="underline" href={data.nextArtPiece.id}>Next</a>
+		{:else}
+			<span>Next</span>
+		{/if}
 	</div>
 	<div>
 		<a href="/">
@@ -23,18 +34,52 @@
 					viewBox="0 0 256 256"
 					><path
 						d="M84,52V92H28a4,4,0,0,1-4-4V64A16,16,0,0,1,40,48H80A4,4,0,0,1,84,52Zm16,152a4,4,0,0,0,4,4h48a4,4,0,0,0,4-4V164H100ZM24,168v24a16,16,0,0,0,16,16H80a4,4,0,0,0,4-4V164H28A4,4,0,0,0,24,168Zm0-56v32a4,4,0,0,0,4,4H84V108H28A4,4,0,0,0,24,112ZM152,48H104a4,4,0,0,0-4,4V92h56V52A4,4,0,0,0,152,48Zm76,60H172v40h56a4,4,0,0,0,4-4V112A4,4,0,0,0,228,108ZM100,148h56V108H100ZM216,48H176a4,4,0,0,0-4,4V92h56a4,4,0,0,0,4-4V64A16,16,0,0,0,216,48Zm12,116H172v40a4,4,0,0,0,4,4h40a16,16,0,0,0,16-16V168A4,4,0,0,0,228,164Z"
-					></path></svg
-				>
+					></path>
+				</svg>
 			</span>
 		</a>
 	</div>
 </nav>
-<section>
+<section class="flex flex-col space-y-8">
 	<h1 class="text-center h2">{'name' in data.piece ? data.piece.name : ''}</h1>
 
-	<img class="mx-auto p-8 max-h-[750px]" src={data.piece.src} alt={data.piece.alt} />
+	<div class="overflow-auto">
+		<img
+			class="mx-auto w-auto h-[200px] sm:h-[400px]"
+			src={selectedImage.src}
+			alt={selectedImage.alt}
+		/>
+	</div>
 
-	{#if 'description' in data.piece}
+	<!-- Details -->
+	<div>
+		{#if data.piece.location}
+			<span class="font-bold"> Address: </span>
+			<a class="underline" target="_blank" href={generateGoogleMapsSearchUrl(data.piece.location)}
+				>{data.piece.location}</a
+			>
+		{/if}
+	</div>
+
+	{#if data.piece.images.length > 1}
+		<div class="w-full flex space-x-4 justify-center">
+			{#each data.piece.images as image, i}
+				<button
+					type="button"
+					on:click={() => (selectedImageIndex = i)}
+					aria-label="Select {image.alt}"
+				>
+					<img
+						class="h-48 w-auto {i === selectedImageIndex ? 'opacity-90' : 'opacity-60'}"
+						src={image.src}
+						alt={image.alt}
+					/>
+				</button>
+			{/each}
+		</div>
+	{/if}
+
+	{#if data.piece.description}
 		<div class="mx-auto prose dark:prose-invert">
 			{#each data.piece.description as line}
 				<p>{line}</p>
