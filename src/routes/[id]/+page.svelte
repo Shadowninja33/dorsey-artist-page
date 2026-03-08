c7a0d6af6fd192ca3070d02bdd8e1d64db84a58b
Shadowninja33 <prestondcobb@gmail.com>
2026-03-08 12:52:16 -0500

    Use gridLayout icon

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { generateGoogleMapsSearchUrl } from '$lib/utils/google-maps';
+	import { LayoutGridIcon } from '@lucide/svelte';
 
 	let { data } = $props();
 
@@ -15,8 +16,8 @@
 	<meta name="description" content={data.seoData.description} />
 </svelte:head>
 
-<nav class="flex justify-between py-4">
-	<div class="flex justify-start">
+<nav class="mx-4 flex justify-between py-4">
+	<div class="flex justify-start text-lg">
 		{#if data.previousArtPiece?.id}
 			<a class="underline" href={data.previousArtPiece.id}>Previous</a>
 		{:else}
@@ -31,18 +32,7 @@
 	</div>
 	<div>
 		<a href="/" aria-label="Back to Gallery">
-			<span>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="32"
-					height="32"
-					class="fill-token"
-					viewBox="0 0 256 256"
-					><path
-						d="M84,52V92H28a4,4,0,0,1-4-4V64A16,16,0,0,1,40,48H80A4,4,0,0,1,84,52Zm16,152a4,4,0,0,0,4,4h48a4,4,0,0,0,4-4V164H100ZM24,168v24a16,16,0,0,0,16,16H80a4,4,0,0,0,4-4V164H28A4,4,0,0,0,24,168Zm0-56v32a4,4,0,0,0,4,4H84V108H28A4,4,0,0,0,24,112ZM152,48H104a4,4,0,0,0-4,4V92h56V52A4,4,0,0,0,152,48Zm76,60H172v40h56a4,4,0,0,0,4-4V112A4,4,0,0,0,228,108ZM100,148h56V108H100ZM216,48H176a4,4,0,0,0-4,4V92h56a4,4,0,0,0,4-4V64A16,16,0,0,0,216,48Zm12,116H172v40a4,4,0,0,0,4,4h40a16,16,0,0,0,16-16V168A4,4,0,0,0,228,164Z"
-					></path>
-				</svg>
-			</span>
+			<LayoutGridIcon class="size-6" />
 		</a>
 	</div>
 </nav>
@@ -53,7 +43,7 @@
 		<enhanced:img
 			fetchpriority="high"
 			class="mx-auto max-h-56 w-auto sm:max-h-96"
-			sizes="min(2560px, 100vw)"
+			sizes="(min-width:1280px) 1280px, (min-width:600px) 200px, 100px"
 			src={selectedImage.src}
 			alt={selectedImage.alt}
 		/>
@@ -89,7 +79,7 @@
 	{/if}
 
 	{#if data.piece.description}
-		<div class="mx-auto prose dark:prose-invert">
+		<div class="prose dark:prose-invert mx-auto">
 			{#each data.piece.description as line}
 				<p>{line}</p>
 			{/each}
